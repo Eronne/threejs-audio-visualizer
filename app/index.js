c89e5beb491645a32648d15ed8df6b44dd9f753e
Eronne <erwann.letue@gmail.com>
2017-10-27 12:50:51 +0200

    Added landing page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import './index.css';
-
 import App from './scripts/App';
 
-window.app = new App();
+let landingPage = document.getElementById('landing')
+let landingButton = document.getElementById('landingButton')
+let controls = document.getElementById('controls')
+
+landingButton.addEventListener('click', () => {
+    landingPage.style.display="none";
+    window.app = new App();
+    controls.classList.remove('hidden');
+})
